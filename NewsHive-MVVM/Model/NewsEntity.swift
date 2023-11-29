d7a503ea256e3f1182ed2629ffd9fb412f1e67ed
Ahmet Enes Irmak <ahmetenesirmak@gmail.com>
2023-11-29 14:47:24 +0300

    Create News Model

--- NewsHive-MVVM/Model/NewsEntity.swift
+++ NewsHive-MVVM/Model/NewsEntity.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+//MARK: - NewsList
+struct NewsList: Codable {
+    let articles: [News]?
+}
+
+//MARK: - News
+struct News: Codable {
+    let source: Source?
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: Date?
+    let content: String?
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String?
+}
